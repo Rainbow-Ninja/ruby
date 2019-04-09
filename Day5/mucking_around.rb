2ea5700f88174e7735c353e12dc3e741668215af
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-04-09 11:51:18 +1000

    blocks and yields

---
+++ Day5/mucking_around.rb
@@ -0,0 +1,34 @@
+# planet = "Earth"
+
+# puts leng = planet.length
+# puts planet[leng-1]
+
+# word = "Cure"
+# rhash = "Cha'n;n:el"
+# puts channelName = rhash["Cha'n;n:el"].gsub(/[':;]/, "")
+
+# word = "CurCeCCCC"
+# puts CureSub = word["CurCeCCCC"].gsub("C", "P")
+# first=last=legal="bart"
+# puts first
+# puts last
+# puts legal
+
+# 'Strawberry' = fav_flavour
+
+# something = "math"
+# subject = something
+# puts something
+
+# puts service = "Cloud".to_i + 9
+# puts "Cloud".to_i
+
+h = {:name => "Jane", :age => 17}
+p true.class, false.class
+p "Ruby".class
+p 1.class
+p 4.5.class
+p 3_463_456_457.class
+p :age.class
+p [1, 2, 3].class
+p h.class
---
+++ Day6/blocksYield.rb
@@ -0,0 +1,79 @@
+# name = ["Alex", "Saad", "Sally"]
+# name.each do |name|
+#     puts "Welcome #{name}"
+# end
+
+# name.each { |name| puts "Welcome #{name}"}
+
+#------------------------------------
+
+#method
+# name = "Alex"
+# def greet(name)
+#     puts "Hello #{name}"
+# end
+# greet(name)
+
+# #block
+# def greet
+#     puts "before yield"
+#     yield
+#     puts "after yield"
+# end
+
+# greet{puts "hello world"}
+# #when pass argument use (), when passing block use {}
+
+# #--------------------------------------
+
+# #yield can take parameters/arguments
+# def add(num1, num2)
+#     sum = num1 + num2
+#     diff = num1 - num2
+#     yield(sum, diff)
+# end
+# add(10,20) {|sum, diff| puts "sum of two numbers is #{sum}"
+# puts "diff of two numbers is #{diff}"}
+# # #--------------------------------------------------
+# def greet
+#     name = "nands"
+#     age = 10
+#     yield(name, age)
+# end
+# greet{|age, name| puts "Hello #{name}, you are #{age}yo"}
+
+# def greet
+#     yield("Alex")
+#     puts "the name inside the method, but outside the block is #{name}"
+# end
+# greet {|name| puts "value of the name in the block is #{name}"}
+
+#---------------------------------------------------
+# name = ["Alex", "Saad", "Sally"]
+# names.each { |name| puts "Welcome #{name}"}
+
+# def my_each(name)
+#     counter = 0
+#     while counter < name.length
+#         yield(name[counter])
+#         counter += 1
+#     end
+# end
+# my_each(name) {|name| puts "Welcome #{name}"}
+
+# names = ["Alex", "Saad", "Sally"]
+
+# names = ["Alex", "Saad", nil, "Sally"]
+# def my_each(names)
+#     for item in names
+#         yield item
+#     end
+# end
+
+# my_each(names){|name| puts "Welcome #{name}"}
+#-------------------------------------------------
+numbers = [10, 2, 3, 5, 7, 18, 12]
+print numbers.find {|num| num>5} #returns first num>5
+print numbers.find_all{|num| num>5} #returns all numbers >5
+print numbers.find_all{|num| num%2==0} #returns all even nums
+print numbers.delete_if{|num| num%2==0} #mutating. Also deletes all even numbers
