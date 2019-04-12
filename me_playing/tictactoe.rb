c087fc60a499fa25aa8cd3354c1dfe2c059abeb7
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-04-12 16:38:30 +1000

    Added comments and cleaned up a bit

--- me_playing/tictactoe.rb
+++ me_playing/tictactoe.rb
@@ -3,7 +3,7 @@ $player = "Player 1"
 no_winner = true
 total_moves = 0
 
-def print_board(moves_so_far)
+def print_board(moves_so_far) #prints the board with current moves included
     puts "         |         |         "
     puts "    #{moves_so_far[0][0]}    |    #{moves_so_far[0][1]}    |    #{moves_so_far[0][2]}    "
     puts "_________|_________|_________"
@@ -15,7 +15,7 @@ def print_board(moves_so_far)
     puts "         |         |         "
 end
 
-def switch_player(total_moves)
+def switch_player(total_moves) #switches between player 1 and player 2
     total_moves +=1
     if total_moves < 9
         if total_moves%2==0
@@ -28,7 +28,7 @@ def switch_player(total_moves)
     return total_moves
 end
 
-def mark_board(moves_so_far, posA, posB)
+def mark_board(moves_so_far, posA, posB) #saves the players choice so it can be marked on the board
     if $player == "Player 1"
         moves_so_far[posA][posB] = "O"
     else
@@ -36,14 +36,14 @@ def mark_board(moves_so_far, posA, posB)
     end
 end
 
-def ask_for_move(moves_so_far)
+def ask_for_move(moves_so_far) #asks player for position to mark. Loops until valid choice
     spot_free = false
 
-    while spot_free == false
+    while spot_free == false #loops if player chooses an already taken spot
         valid_row = false
         valid_column = false
 
-        while valid_row == false
+        while valid_row == false #loops until player gives valid row choice
             puts "\nWould you like to go in the \n1. Top row \n2. Middle row or \n3. Bottom row?\n(type 1, 2 or 3) "
             row = gets.chomp.to_i
             if row == 1
@@ -59,7 +59,7 @@ def ask_for_move(moves_so_far)
                 puts "\n*** Invalid option. Try again ***"
             end
         end
-        while valid_column == false
+        while valid_column == false #loops until player gives valid column choice
             puts "\nWould you like to go on the \n1. Left \n2. Middle or \n3. Right of the row?\n(type 1, 2 or 3) "
             column = gets.chomp.to_i
             if column == 1
@@ -75,9 +75,8 @@ def ask_for_move(moves_so_far)
                 puts "\n*** Invalid option. Try again ***"
             end
         end
-
-        # \while spot_free == false    
-        if  moves_so_far[posA][posB] == "-"
+   
+        if  moves_so_far[posA][posB] == "-" #makes sure spot is actually free then marks it
             mark_board(moves_so_far, posA, posB)
             print_board(moves_so_far)
             spot_free = true
@@ -88,7 +87,7 @@ def ask_for_move(moves_so_far)
 end
 
 
-def cool_end
+def cool_end #does cool flashy stars for the winner
     start = 0
     stars1 = "*  *  *  *  *  *  *  *  *  *  *  *  *\n\n           #{$player} Wins!!\n\n  *  *  *  *  *  *  *  *  *  *  *  * "
     stars2 = " *  *  *  *  *  *  *  *  *  *  *  *  \n\n           #{$player} Wins!!\n\n *  *  *  *  *  *  *  *  *  *  *  *  "
@@ -109,7 +108,7 @@ def cool_end
     end
 end
 
-def winner(moves_so_far)
+def winner(moves_so_far) #checks to see if either player has won yet
     if ((moves_so_far[0][0]==moves_so_far[0][1]&&moves_so_far[0][0]==moves_so_far[0][2]))&&((moves_so_far[0][0]!="-"&&moves_so_far[0][1]!="-"&&moves_so_far[0][2]!="-"))||((moves_so_far[1][0]==moves_so_far[1][1]&&moves_so_far[1][0]==moves_so_far[1][2]))&&((moves_so_far[1][0]!="-"&&moves_so_far[1][1]!="-"&&moves_so_far[1][2]!="-"))||((moves_so_far[2][0]==moves_so_far[2][1]&&moves_so_far[2][0]==moves_so_far[2][2]))&&((moves_so_far[2][0]!="-"&&moves_so_far[2][1]!="-"&&moves_so_far[2][2]!="-"))||((moves_so_far[0][0]==moves_so_far[1][0]&&moves_so_far[0][0]==moves_so_far[2][0]))&&((moves_so_far[0][0]!="-"&&moves_so_far[1][0]!="-"&&moves_so_far[2][0]!="-"))||((moves_so_far[0][1]==moves_so_far[1][1]&&moves_so_far[0][1]==moves_so_far[2][1]))&&((moves_so_far[0][1]!="-"&&moves_so_far[1][1]!="-"&&moves_so_far[2][1]!="-"))||((moves_so_far[0][2]==moves_so_far[1][2]&&moves_so_far[0][2]==moves_so_far[2][2]))&&((moves_so_far[0][2]!="-"&&moves_so_far[1][2]!="-"&&moves_so_far[2][2]!="-"))||((moves_so_far[0][0]==moves_so_far[1][1]&&moves_so_far[0][0]==moves_so_far[2][2]))&&((moves_so_far[0][0]!="-"&&moves_so_far[1][1]!="-"&&moves_so_far[0][0]!="-"))||((moves_so_far[0][2]==moves_so_far[1][1]&&moves_so_far[0][2]==moves_so_far[2][0]))&&((moves_so_far[0][2]!="-"&&moves_so_far[1][1]!="-"&&moves_so_far[0][2]!="-"))
        cool_end
        return false
@@ -118,6 +117,7 @@ def winner(moves_so_far)
     end
 end
 
+#-------------------------------------------------------------------------------------------
 
 print_board(moves_so_far)
 puts "** Player 1: you are naughts. Player 2: you are crosses **"
@@ -129,6 +129,6 @@ while no_winner&&(total_moves<9)
         total_moves = switch_player(total_moves)
     end
 end
-if (total_moves == 9)
+if (total_moves == 9) #if there have been 9plays and no wins
     puts "\n** We have a Draw! **"
 end
