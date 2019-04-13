b1c97217f9980507e3bebdcf2425186b408280e7
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-04-14 00:05:28 +1000

    PacMan - cant got through walls, go through door, colours

--- me_playing/pacman.rb
+++ me_playing/pacman.rb
@@ -3,96 +3,128 @@ $posA = 5
 $posB = 0
 current_spot = board[$posA][$posB]
 $score = 0
+$count
 
-board [0] = ["-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-"]
-board [1] = ["|", "O", "o", "o", "o", "|", "o", "o", "o", "o", "o","o","o","o", "|", "o", "o", "o", "O", "|"]
-board [2] = ["|", "o", "-", "-", "o", "|", "o", "-", "-", "-", "-", "-", "-", "o", "|", "o", "-", "-", "o", "|"]
-board [3] = ["|", "o", "|", "o", "o", "o", "o", "o", "o", "o", "o","o","o","o", "o", "o", "o", "|", "o", "|"]
-board [4] = ["|", "o", "|", "o", "-", "-", "o", "-", "-", " ", " ","-","-","o", "-", "-", "o", "|", "o", "|"]
-board [5] = ["C", "o", "o", "o", "o", "o", "o", "|", " ", " ", " ", " ", "|", "o", "o", "o", "o", "o", "o", " "]
-board [6] = ["|", "o", "|", "o", "-", "-", "o", "-", "-", "-", "-","-","-","o", "-", "-", "o", "|", "o", "|"]
-board [7] = ["|", "o", "|", "o", "o", "o", "o", "o", "o", "o", "o","o","o","o", "o", "o", "o", "|", "o", "|"]
-board [8] = ["|", "o", "-", "-", "o", "|", "o", "-", "-", "-", "-", "-", "-", "o", "|", "o", "-", "-", "o", "|"]
-board [9] = ["|", "O", "o", "o", "o", "|", "o", "o", "o", "o", "o","o","o","o", "|", "o", "o", "o", "O", "|"]
-board [10] = ["-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-"]
+class String
+    def gold; "\e[33m#{self}\e[0m" end
+    def magenta; "\e[35m#{self}\e[0m" end
+    def bold; "\e[1m#{self}\e[22m" end
+    def cyan; "\e[36m#{self}\e[0m" end
+    def red; "\e[31m#{self}\e[0m" end
+    def blue; "\e[34m#{self}\e[0m" end
+    def green; "\e[32m#{self}\e[0m" end
+
+    def bg_red; "\e[41m#{self}\e[0m" end
+    def bg_green; "\e[42m#{self}\e[0m" end
+    def bg_gold; "\e[43m#{self}\e[0m" end
+    def bg_blue; "\e[44m#{self}\e[0m" end
+    def bg_magenta; "\e[45m#{self}\e[0m" end
+end
+
+board [0] = ["-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan]
+board [1] = ["|".cyan, "O".red, "o", "o", "o", "|".cyan, "o", "o", "o", "o", "o","o","o","o", "|".cyan, "o", "o", "o", "O".red, "|".cyan]
+board [2] = ["|".cyan, "o", "-".cyan, "-".cyan, "o", "|".cyan, "o", "-".cyan, "-".cyan, "-".cyan, "-".cyan, "-".cyan, "-".cyan, "o", "|".cyan, "o", "-".cyan, "-".cyan, "o", "|".cyan]
+board [3] = ["|".cyan, "o", "|".cyan, "o", "o", "o", "o", "o", "o", "o", "o","o","o","o", "o", "o", "o", "|".cyan, "o", "|".cyan]
+board [4] = ["|".cyan, "o", "|".cyan, "o", "-".cyan, "-".cyan, "o", "-".cyan, "-".cyan, " ", " ","-".cyan,"-".cyan,"o", "-".cyan, "-".cyan, "o", "|".cyan, "o", "|".cyan]
+board [5] = ["c".gold, "o", "o", "o", "o", "o", "o", "|".cyan, "n".bg_red, "n".bg_blue, "n".bg_green, "n".bg_magenta, "|".cyan, "o", "o", "o", "o", "o", "o", " "]
+board [6] = ["|".cyan, "o", "|".cyan, "o", "-".cyan, "-".cyan, "o", "-".cyan, "-".cyan, "-".cyan, "-".cyan,"-".cyan,"-".cyan,"o", "-".cyan, "-".cyan, "o", "|".cyan, "o", "|".cyan]
+board [7] = ["|".cyan, "o", "|".cyan, "o", "o", "o", "o", "o", "o", "o", "o","o","o","o", "o", "o", "o", "|".cyan, "o", "|".cyan]
+board [8] = ["|".cyan, "o", "-".cyan, "-".cyan, "o", "|".cyan, "o", "-".cyan, "-".cyan, "-".cyan, "-".cyan, "-".cyan, "-".cyan, "o", "|".cyan, "o", "-".cyan, "-".cyan, "o", "|".cyan]
+board [9] = ["|".cyan, "O".red, "o", "o", "o", "|".cyan, "o", "o", "o", "o", "o","o","o","o", "|".cyan, "o", "o", "o", "O".red, "|".cyan]
+board [10] = ["-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan,"-".cyan]
 
 def print_board(board)
+    system("clear")
     puts board.map { |x| x.join(' ') }
-    puts "\n Score is  #{$score}"
+    puts "\nScore is  #{$score}"
     puts "\nHit 'q' to quit "
 end
 
 def current_score(current_spot)
     if current_spot == "o"
-        return $score += 10
+        $score += 10
+        $count += 1 #use later to annouce winning
     end
 end
 
 def move_up(board, current_spot)
-    system("clear")
-    if (board[$posA-1][$posB] == "-")||(board[$posA-1][$posB] == "|")
+    if (board[$posA-1][$posB] == "-".cyan)||(board[$posA-1][$posB] == "|".cyan)
         print_board(board)
     else
-        board[$posA][$posB] = "."
+        board[$posA][$posB] = " "
         $posA = $posA-1
         current_spot = board[$posA][$posB]
         current_score(current_spot)
-        board[$posA][$posB] = "C"
+        board[$posA][$posB] = "c".gold
         print_board(board)
     end
     return current_spot
 end
 
 def move_down(board, current_spot)
-    system("clear")
-    if (board[$posA+1][$posB] == "-")||(board[$posA+1][$posB] == "|")
+    if (board[$posA+1][$posB] == "-".cyan)||(board[$posA+1][$posB] == "|".cyan)
         print_board(board)
     else
-        board[$posA][$posB] = "."
+        board[$posA][$posB] = " "
         $posA = $posA+1
         current_spot = board[$posA][$posB]
         current_score(current_spot)
-        board[$posA][$posB] = "C"
+        board[$posA][$posB] = "c".gold
         print_board(board)
     end
     return current_spot
 end
 
 def move_right(board, current_spot)
-    system("clear")
-    if (board[$posA][$posB + 1] == "-")||(board[$posA][$posB + 1] == "|")
+    if (board[$posA][$posB + 1] == "-".cyan)||(board[$posA][$posB + 1] == "|".cyan)
+        print_board(board)
+    elsif $posA == 5 && $posB == 19 #if board is at edge and \door go through
+        board[$posA][$posB] = " "
+        $posB = $posB - 19
+        current_spot = board[$posA][$posB]
+        current_score(current_spot)
+        board[$posA][$posB] = "c".gold
         print_board(board)
     else
-        board[$posA][$posB] = "."
+        board[$posA][$posB] = " "
         $posB = $posB + 1
         current_spot = board[$posA][$posB]
         current_score(current_spot)
-        board[$posA][$posB] = "C"
+        board[$posA][$posB] = "c".gold
         print_board(board)
     end
     return current_spot
 end
 
 def move_left(board, current_spot)
-    system("clear")
-    if (board[$posA][$posB - 1] == "-")||(board[$posA][$posB - 1] == "|")
+    if (board[$posA][$posB - 1] == "-".cyan)||(board[$posA][$posB - 1] == "|".cyan)
+        print_board(board)
+    elsif $posA == 5 && $posB == 0 #if board is at edge and \door go through
+        board[$posA][$posB] = " "
+        $posB = $posB + 19
+        current_spot = board[$posA][$posB]
+        current_score(current_spot)
+        board[$posA][$posB] = "c".gold
         print_board(board)
     else
-        board[$posA][$posB] = "."
+        board[$posA][$posB] = " "
         $posB = $posB-1
         current_spot = board[$posA][$posB]
         current_score(current_spot)
-        board[$posA][$posB] = "C"
+        board[$posA][$posB] = "c".gold
         print_board(board)
     end
     return current_spot
+end
+
+def pill
+    
 end
 
 def conflict
 
 end
 
-system("clear")
 print_board(board)
 require 'io/console'
 loop do
